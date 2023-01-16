6ffd705db26faa64f1ab76e1a8c1351f9f86b359
Alessandro Schwaiger <aless2003@gmx.at>
2023-01-16 22:57:08 +0100

    Multithreading support to increase Performance

--- src/main/java/com/diamond/badApple/FrameResizer.java
+++ src/main/java/com/diamond/badApple/FrameResizer.java
@@ -8,6 +8,11 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javax.imageio.ImageIO;
 import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
@@ -33,29 +38,56 @@ public class FrameResizer {
       return;
     }
 
+    ExecutorService executor = Executors.newFixedThreadPool(16);
+
     ProgressBarBuilder pbb = new ProgressBarBuilder()
         .setTaskName("Resizing frames")
         .setInitialMax(frames.length)
         .setStyle(ProgressBarStyle.ASCII);
 
     try (var pb = pbb.build()) {
+
+      List<Future<?>> futures = new ArrayList<>();
+
+
       for (File frame : frames) {
-        BufferedImage image = ImageIO.read(frame);
-        float aspectRatio = (float) image.getWidth() / image.getHeight();
-        int newWidth = resizedWidth;
-        int newHeight = (int) (newWidth / aspectRatio);
-
-        Image resized = image.getScaledInstance(newWidth, newHeight, SCALE_SMOOTH);
-
-        BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, TYPE_INT_RGB);
-
-        Graphics2D graphics = scaledImage.createGraphics();
-        graphics.drawImage(resized, 0, 0, null);
-        graphics.dispose();
-
-        ImageIO.write(scaledImage, "png", frame);
-        pb.step();
+        var future = executor.submit(() -> resizeFrame(frame));
+        futures.add(future);
       }
+
+      while (!futures.isEmpty()) {
+        var iterator = futures.iterator();
+
+        while (iterator.hasNext()) {
+          var future = iterator.next();
+          if (future.isDone()) {
+            iterator.remove();
+            pb.step();
+          }
+        }
+      }
+    }
+
+    executor.shutdown();
+
+  }
+
+  private void resizeFrame(File frame) {
+    try {
+      BufferedImage image = ImageIO.read(frame);
+      float aspectRatio = (float) image.getWidth() / image.getHeight();
+      int newWidth = resizedWidth;
+      int newHeight = (int) (newWidth / aspectRatio);
+
+      Image resized = image.getScaledInstance(newWidth, newHeight, SCALE_SMOOTH);
+
+      BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, TYPE_INT_RGB);
+
+      Graphics2D graphics = scaledImage.createGraphics();
+      graphics.drawImage(resized, 0, 0, null);
+      graphics.dispose();
+
+      ImageIO.write(scaledImage, "png", frame);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
