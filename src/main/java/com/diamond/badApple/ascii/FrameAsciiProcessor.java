25dc263fc242e9ba542d8ad5a617296d1bbfb236
Alessandro Schwaiger <46018087+aless2003@users.noreply.github.com>
2023-01-17 20:01:42 +0100

    - Dynamic Frame Time calculation - Added Use of ConsoleUtils#clearScreen() - Added Exception in case executor doesn't terminate

--- src/main/java/com/diamond/badApple/ascii/FrameAsciiProcessor.java
+++ src/main/java/com/diamond/badApple/ascii/FrameAsciiProcessor.java
@@ -29,10 +29,12 @@ public class FrameAsciiProcessor {
   private int counter = 0;
   private final boolean audio;
   ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+  private final long frameTime;
 
-  public FrameAsciiProcessor(AudioPlayer audioPlayer, boolean audio) {
+  public FrameAsciiProcessor(AudioPlayer audioPlayer, boolean audio, double fps) {
     this.audioPlayer = audioPlayer;
     this.audio = audio;
+    this.frameTime = (long) (1000000 / fps);
   }
 
   public void convertAndPrint(File dir) {
@@ -59,6 +61,7 @@ public class FrameAsciiProcessor {
       throw new RuntimeException(e);
     }
 
+    ConsoleUtils.clearScreen();
     logger.info("Finished in {} time", stopWatch.formatTime());
   }
 
@@ -68,9 +71,15 @@ public class FrameAsciiProcessor {
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
-    executor.scheduleAtFixedRate(this::printNext, 0, 33299, TimeUnit.MICROSECONDS);
+    executor.scheduleAtFixedRate(this::printNext, 0, frameTime, TimeUnit.MICROSECONDS);
     try {
-      executor.awaitTermination(1, TimeUnit.DAYS);
+
+      boolean terminate = executor.awaitTermination(1, TimeUnit.DAYS);
+
+      if (!terminate) {
+        throw new RuntimeException("Failed to terminate");
+      }
+
     } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
@@ -148,6 +157,8 @@ public class FrameAsciiProcessor {
       logger.error("Couldn't Process frames", e);
       throw new RuntimeException(e);
     }
+
+    ConsoleUtils.clearScreen();
   }
 
 }
